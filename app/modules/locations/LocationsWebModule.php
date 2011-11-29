aaec1702ed74c60d2e2fbf43f0f69953efc57e5a
wang Brent <Brent@wang-Brents-Mac-mini.local>
2011-11-29 14:46:45 +0800

    create getLocationFeed method to get the dataModel for each location

--- app/modules/locations/LocationsWebModule.php
+++ app/modules/locations/LocationsWebModule.php
@@ -6,6 +6,18 @@ class LocationsWebModule extends WebModule
     
     protected $feeds = array();
     
+    
+    public function getLocationFeed($id) {
+        if (!isset($this->feeds[$id])) {
+            throw new KurogoDataException('Unable to load data for location '. $id);
+        }
+        
+        $feedData = $this->feeds[$id];
+        $dataModel = isset($feedData['MODEL_CLASS']) ? $feedData['MODEL_CLASS'] : 'LocationsDataModel';
+        
+        return LocationsDataModel::factory($dataModel, $feedData);
+    }
+    
     protected function initialize() {
         $this->feeds = $this->loadFeedData();
     } 
@@ -17,7 +29,10 @@ class LocationsWebModule extends WebModule
             case 'index':
                 $locations = array();
                 
-                
+                foreach ($this->feeds as $id => $feedData) {
+                    $feed = $this->getLocationFeed($id);
+                    print_r($feed);
+                }
                 print_r($this->feeds);
                 exit;
                 break;
