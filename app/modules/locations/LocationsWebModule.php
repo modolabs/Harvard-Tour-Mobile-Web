42fe09866d2b3e0a3b9dd07fe07d5e94c0a6afe7
wang Brent <Brent@wang-Brents-Mac-mini.local>
2011-11-29 14:34:43 +0800

    create a feed.ini for the each location

--- app/modules/locations/LocationsWebModule.php
+++ app/modules/locations/LocationsWebModule.php
@@ -4,6 +4,23 @@ class LocationsWebModule extends WebModule
 {
     protected $id = 'locations';
     
+    protected $feeds = array();
+    
+    protected function initialize() {
+        $this->feeds = $this->loadFeedData();
+    } 
+    
     protected function initializeForPage() {
+        
+        switch ($this->page) {
+            
+            case 'index':
+                $locations = array();
+                
+                
+                print_r($this->feeds);
+                exit;
+                break;
+        }
     }
 }
