65ef4a32767bdc2b492c9e116bc2b012bba9d858
Jeffery <i@youjf.com>
2011-11-29 18:03:56 +0800

    KGO-203: implemented detail page for locations module

--- app/modules/locations/LocationsWebModule.php
+++ app/modules/locations/LocationsWebModule.php
@@ -1,7 +1,6 @@
 <?php
 
-class LocationsWebModule extends WebModule
-{
+class LocationsWebModule extends WebModule {
     protected $id = 'locations';
     
     protected $feeds = array();
@@ -20,6 +19,7 @@ class LocationsWebModule extends WebModule
     
     protected function initialize() {
         $this->feeds = $this->loadFeedData();
+        $this->timezone = Kurogo::siteTimezone();
     } 
     
     protected function initializeForPage() {
@@ -36,6 +36,49 @@ class LocationsWebModule extends WebModule
                 }
                 print_r($this->feeds);
                 exit;
+                break;
+            case 'detail':
+                $id = $this->getArg('id');
+                // specified date for events
+                $date = $this->getArg('date', date('Y-m-d', time()));
+                $feed = $this->getLocationFeed($id);
+                // get title, subtitle and maplocation
+                $title = $feed->getTitle();
+                $subtitle = $feed->getSubtitle();
+                $mapLocation = $feed->getMapLocation();
+                $start = new DateTime($date, $this->timezone);
+                $end = clone $start;
+                $start->setTime(0,0,0);
+                $end->setTime(23,59,59);
+                // set start and end date for items
+                $feed->setStartDate($start);
+                $feed->setEndDate($end);
+                $items = $feed->items();
+                $events = array();
+                // format events data
+                foreach($items as $item) {
+                    $event['title'] = $item->get_summary();
+                    $event['subtitle'] = date("H:i:s", $item->get_start()) . " - " . date("H:i:s", $item->get_end());
+                    $events[] = $event;
+                }
+                $nextDate = date("Y-m-d", strtotime("+1 day", strtotime($date)));
+                $nextDateString = date("F j", strtotime("+1 day", strtotime($date)));
+                $nextDetail = array(
+                    'title' => "See next day's info",
+                    'url' => $this->buildBreadcrumbURL('detail', array('id' => $id, 'date' => $nextDate), true)
+                );
+                $map = Kurogo::moduleLinkForValue('map', $mapLocation, $this);
+                // change tile for the map link
+                $mapLink['title'] = "Search on Map";
+                $mapLink['url'] = $map['url'];
+                $title = array(
+                    'title' => $title,
+                    'subtitle' => $subtitle
+                );
+                $this->assign('title', $title);
+                $this->assign('nextDetail', $nextDetail);
+                $this->assign('mapLink', $mapLink);
+                $this->assign('events', $events);
                 break;
         }
     }
--- lib/LocationsDataModel.php
+++ lib/LocationsDataModel.php
@@ -2,8 +2,32 @@
 
 includePackage('Calendar');
 class LocationsDataModel extends CalendarDataModel {
+    protected $subtitle;
+    protected $mapLocation;
 
     protected function init($args) {
         parent::init($args);
-    }   
+        $this->setSubtitle($args);
+        $this->setMapLocation($args);
+    }
+
+    public function setSubtitle($args) {
+        if(isset($args['SUBTITLE']) && strlen($args['SUBTITLE']) > 0) {
+            $this->subtitle = $args['SUBTITLE'];
+        }
+    }
+
+    public function getSubtitle() {
+        return $this->subtitle;
+    }
+
+    public function setMapLocation($args) {
+        if(isset($args['MAP_LOCATION']) && strlen($args['MAP_LOCATION']) > 0) {
+            $this->mapLocation = $args['MAP_LOCATION'];
+        }
+    }
+
+    public function getMapLocation() {
+        return $this->mapLocation;
+    }
 }
