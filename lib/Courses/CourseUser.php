7f4ba750310915747d5e221c726d053a717c2d99
wang Brent <Brent@wang-Brents-Mac-mini.local>
2012-04-27 16:18:02 +0800

    create isInstructor() / isStudent() methods in CourseContentCourse [KGO-848]

---
+++ lib/Courses/CourseUser.php
@@ -0,0 +1,71 @@
+<?php
+
+class CourseUser implements KurogoObject {
+	protected $id;
+	protected $firstName;
+	protected $lastName;
+	protected $fullName;
+	protected $email; 
+	
+	public function filterItem($filters) {
+		return $filters;
+	}
+	
+	public function setUserID($id) {
+	    $this->id = $id;
+	}
+	
+	public function getUserID() {
+	    return $this->id;
+	}
+	
+	public function getId() {
+		return $this->id;
+	}
+	
+	public function setId($id) {
+		$this->id = $id;
+	}
+	
+	public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+    }
+	
+    public function getFullName() {
+        if (!empty($this->fullName)) {
+            return $this->fullName;
+        } elseif (!empty($this->firstName) || !empty($this->lastName)) {
+            return trim(sprintf("%s %s", $this->firstName, $this->lastName));
+        } else {
+            return $this->getUserID();
+        }
+    }
+
+	public function getEmail() {
+		return $this->email;
+	}
+	
+	public function setEmail($email) {
+		$this->email = $email;
+	}
+}
