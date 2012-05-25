92d058b93c3a36af2e88664096a34f5df1f44bec
Jeffery <i@youjf.com>
2012-05-25 15:28:47 +0800

    KGO-1048: "By Date" for resources and tasks should limit the results

--- app/modules/courses/CoursesWebModule.php
+++ app/modules/courses/CoursesWebModule.php
@@ -866,8 +866,13 @@ class CoursesWebModule extends WebModule {
                 $resourcesLinks = array();
                 $resourcesOptions = $this->getOptionsForResources($options);
                 $groups = $contentCourse->getResources($resourcesOptions);
-                $limit = $resourcesOptions['limit'];
                 $group = $resourcesOptions['group'];
+                if($group == "date") {
+                    $limit = 0;
+                    $pageSize = $resourcesOptions['limit'];
+                }else {
+                    $limit = $resourcesOptions['limit'];
+                }
                 $key = $resourcesOptions['key'];
                 $seeAllLinks = array();
 
@@ -900,7 +905,7 @@ class CoursesWebModule extends WebModule {
                         'items' => $groupItems,
                         'count' => count($items),
                     );
-                    if(count($items) > $limit && $limit != 0){
+                    if($group != "date" && count($items) > $limit && $limit != 0){
                         $courseOptions = $this->getCourseOptions();
                         $courseOptions['group'] = $group;
                         $courseOptions['key'] = $groupTitle;
@@ -908,6 +913,12 @@ class CoursesWebModule extends WebModule {
                         $resource['url'] = $this->buildBreadcrumbURL("resourceSeeAll", $courseOptions, false);
                     }
                     $resourcesLinks[] = $resource;
+                }
+                if($group == "date" && $pageSize && isset($resourcesLinks[0])) {
+                    $resource = $resourcesLinks[0];
+                    $limitedItems = $this->paginateArray($resource['items'], $pageSize, "RESOURCES_DATE", $tab);
+                    $resourcesLinks[0]['items'] = $limitedItems;
+                    $resourcesLinks[0]['count'] = count($limitedItems);
                 }
 
                 $this->assign('resourcesLinks',$resourcesLinks);
