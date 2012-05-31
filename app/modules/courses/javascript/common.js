f385e961ee29472fa50b820eec2bf41f0d287938
Pete Akins <pete.akins@modolabs.com>
2012-05-31 08:54:46 -0400

    don't load tabs more than once

--- app/modules/courses/javascript/common.js
+++ app/modules/courses/javascript/common.js
@@ -2,12 +2,20 @@ function loadSection(select, page) {
     window.location = "./" + page + "?term=" + select.value;
 }
 
+var tabsLoaded = {};
+
 function updateTab(tab, contentURL) {
-//    console.log('updating ' + tab + ' to '+ contentURL);
+    if (tab in tabsLoaded) {
+        return;
+    }
+//    console.log('loading ' + tab + ' from ' + contentURL);
     var element = document.getElementById(tab + '-tabbody');
     ajaxContentIntoContainer({ 
         url: contentURL, // the url to get the page content from 
         container: element, // the container to dump the content into 
-        timeout: 10 // how long to wait for the server before returning an error 
+        timeout: 10, // how long to wait for the server before returning an error 
+        success: function() { tabsLoaded[tab] = true; },
+        error: function() { element.innerHTML = 'Error loading content';}
+        
     });
 }
