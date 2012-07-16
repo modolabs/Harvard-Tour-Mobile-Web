4c008aefd57cae4319bcc2c2d45e5f4ef5d5cdb4
wang Brent <Brent@CD003-wangxi.local>
2012-07-16 16:55:59 +0800

    Update Locations API to support feed groups [KGO-1407]

--- app/modules/locations/LocationsAPIModule.php
+++ app/modules/locations/LocationsAPIModule.php
@@ -17,13 +17,13 @@ class LocationsAPIModule extends APIModule {
     
     protected $feeds = array();
     protected $timezone;
+    protected $feedGroups = array();
+    protected $feedGroup;
 
     public function getLocationFeed($id) {
-        if (!isset($this->feeds[$id])) {
+        if (!$feedData = $this->getFeed($id)) {
             throw new KurogoDataException($this->getLocalizedString('ERROR_NO_LOCATION_FEED', $id));
         }
-        
-        $feedData = $this->feeds[$id];
         $dataModel = isset($feedData['MODEL_CLASS']) ? $feedData['MODEL_CLASS'] : 'LocationsDataModel';
         
         return LocationsDataModel::factory($dataModel, $feedData);
@@ -52,10 +52,100 @@ class LocationsAPIModule extends APIModule {
         );
     }
 
+    public function getFeedGroups() {
+        if ($feedGroups = $this->getOptionalModuleSections('feedgroups')) {
+            return $feedGroups;
+        } else {
+            return array(
+                'nogroup' => array(
+                    'title' => ''
+                )
+            );
+        }
+    }
+    
+    public function loadFeedData($groupID = NULL) {
+        if ($groupID == 'nogroup') {
+            return parent::loadFeedData();
+        } else {
+            $configName = "feeds-$groupID";
+            return $this->getModuleSections($configName);
+        }
+    }
+
+    public static function locationIdForFeedData(Array $feedData) {
+        $identifier = Kurogo::arrayVal($feedData, 'TITLE', '');
+        if (isset($feedData['BASE_URL'])) {
+            $identifier .= $feedData['BASE_URL'];
+        }
+        return substr(md5($identifier), 0, 10);
+    }
+
+    protected function getFeedsData() {
+        foreach ($this->feedGroups as $groupID => &$feedGroup) {
+            $feeds = $this->loadFeedData($groupID);
+            $feedGroup['locations'] = array();
+            foreach ($feeds as $id => $feedData) {
+                $feed = array(
+                    'id'=>self::locationIdForFeedData($feedData),
+                    'title'=>$feedData['TITLE'],
+                    'subtitle'=>isset($feedData['SUBTITLE']) ? $feedData['SUBTITLE'] : "",
+                    'maplocation'=>$feedData['MAP_LOCATION'],
+                    'description'=>isset($feedData['DESCRIPTION']) ? $feedData['DESCRIPTION'] : ""
+                );
+                $feedGroup['locations'][] = $feed;
+            }
+        }
+
+        $feeds = array();
+        if ($this->requestedVersion <= 1) {
+            foreach ($this->feedGroups as $groupID => $feedGroup) {
+                $feeds = array_merge($feeds, $feedGroup['locations']);
+            }
+        } else {
+            $feeds = $this->feedGroups;
+        }
+        
+        return $feeds;
+    }
+
+    protected function getFeed($requestedFeedId) {
+        $feed = array();
+
+        if ($this->feedGroup !== NULL) {
+            $feeds = $this->loadFeedData($this->feedGroup);
+            foreach ($feeds as $id => $feedData) {
+                if ($requestedFeedId == self::locationIdForFeedData($feedData)) {
+                    $feed = $feedData;
+                    break;
+                }
+            }
+        } else {
+            foreach ($this->feedGroups as $groupID => $groupData) {
+                $feeds = $this->loadFeedData($groupID);
+                foreach ($feeds as $id => $feedData) {
+                    if ($requestedFeedId == self::locationIdForFeedData($feedData)) {
+                        $feed = $feedData;
+                        break;
+                    }
+                }
+                if ($feed) {
+                    break;
+                }
+            }
+        }
+
+        return $feed;
+    }
+
     public function initializeForCommand() {
-       	$this->setResponseVersion(1);
+       	//$this->setResponseVersion(1);
        	$this->timezone = Kurogo::siteTimezone();
-       	$this->feeds = $this->loadFeedData();
+       	//$this->feeds = $this->loadFeedData();
+        $this->feedGroups = $this->getFeedGroups();
+        $this->requestedVersion = $this->requestedVersion < 2 ? 1 : 2;
+        $this->feedGroup = $this->getArg('group', NULL);
+
         switch ($this->command) {
             case 'schedule':
             	$id = $this->getArg('id');
@@ -82,7 +172,9 @@ class LocationsAPIModule extends APIModule {
                 }
             	$response['events'] = $events;
                 $this->setResponse($response);
+                $this->setResponseVersion($this->requestedVersion);
                 break;
+
             case 'status':
                 $id = $this->getArg('id');
                 $feedObject = $this->getLocationFeed($id);
@@ -107,24 +199,19 @@ class LocationsAPIModule extends APIModule {
                 }
                 
                 $this->setResponse($response);
+                $this->setResponseVersion($this->requestedVersion);
                 break;
             
             case 'index':
-            	foreach($this->feeds as $id => $feedData){
-            		$feed = array(
-            			'id'=>$feedData['INDEX'],
-            			'title'=>$feedData['TITLE'],
-            			'subtitle'=>isset($feedData['SUBTITLE']) ? $feedData['SUBTITLE'] : "",
-	            		'maplocation'=>$feedData['MAP_LOCATION'],
-	            		'description'=>isset($feedData['DESCRIPTION']) ? $feedData['DESCRIPTION'] : ""
-	            	);
-            		$feeds[] = $feed;
-            	}
+                $feeds = $this->getFeedsData();
+
             	$response = array(
             	    'description'=>$this->getModuleVar('description','strings'),
             	    'locations'=>$feeds
             	);
+
             	$this->setResponse($response);
+                $this->setResponseVersion($this->requestedVersion);
             	break;
             default:
                 $this->invalidCommand();
