5ac91a305f5be01e676eb799a55950fc2589fe35
Ebrahim Kobeissi <eebs@eebs-tech.com>
2012-09-28 10:02:08 -0400

    KGO-2066: Added error message to response

--- lib/DataRetriever/DatabaseDataRetriever.php
+++ lib/DataRetriever/DatabaseDataRetriever.php
@@ -14,6 +14,7 @@ class DatabaseDataRetriever extends DataRetriever
     protected $connection;
     protected $sql;
     protected $parameters=array();
+    protected $errorMsg;
     
     protected function init($args) {
         parent::init($args);
@@ -61,6 +62,7 @@ class DatabaseDataRetriever extends DataRetriever
 
         $this->initRequestIfNeeded();
         $response = $this->initResponse();
+        $response->setResponseError($this->errorMsg);
         $response->setStartTime(microtime(true));
 
         if ($sql = $this->sql()) {
