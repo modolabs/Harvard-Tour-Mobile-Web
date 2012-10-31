e7966def17232aba1256429490b0695c941ae813
Ebrahim Kobeissi <eebs@eebs-tech.com>
2012-10-31 18:09:27 -0400

    KGO-2520: Updated transit api
    Conflicts:
    	app/modules/transit/TransitAPIModule.php

--- app/modules/transit/TransitAPIModule.php
+++ app/modules/transit/TransitAPIModule.php
@@ -14,6 +14,7 @@ class TransitAPIModule extends APIModule {
     protected $vmin = 1;
     protected $vmax = 3;
     protected $defaultNewsModel = 'TransitNewsDataModel';
+    protected $collapseRouteTabs = false;
     
     protected function formatBriefRouteInfo($routeId, $routeInfo) {
         return array(
@@ -164,7 +165,7 @@ class TransitAPIModule extends APIModule {
             'id'         => $vehicleId,
             'agency'     => $vehicleInfo['agency'],
             'routeId'    => $vehicleInfo['routeID'],
-            'lastSeen'   => time() + $vehicleInfo['secsSinceReport'],
+            'lastSeen'   => time() - $vehicleInfo['secsSinceReport'],
             'heading'    => $vehicleInfo['heading'],
             'coords'     => array(
                 'lat' => $vehicleInfo['lat'],
@@ -198,207 +199,224 @@ class TransitAPIModule extends APIModule {
         $view = DataModel::factory("TransitViewDataModel", $this->loadFeedData());
         
         switch($this->command) {
-          case 'info':
-            $keyRemap = array(
-                'titles'    => 'title',
-                'subtitles' => 'subtitle',
-                'urls'      => 'url',
-                'classes'   => 'class',
-                'infokeys'  => 'content',
-            );
-          
-            $infoText = $this->getModuleSections('feeds-info');
-            $info = $this->getModuleSections('api-index');
-            
-            $agencies = array();
-            foreach ($info['agencies'] as $agencyID => $agencyName) {
-                $agencies[] = array(
-                    'id'    => $agencyID,
-                    'title' => $agencyName,
+            case 'info':
+                $config = $this->getModuleSection('module');
+                if(isset($config['collapse_route_tabs'])){
+                    $this->collapseRouteTabs = $config['collapse_route_tabs'];
+                }
+                $keyRemap = array(
+                    'subtitles' => 'subtitle',
+                    'urls'      => 'url',
+                    'classes'   => 'class',
+                    'infokeys'  => 'content',
                 );
-            }
-            
-            $results = array(
-                'agencies' => $agencies,
-                'sections' => array(),
-            );
-            
-            foreach ($info['infosections'] as $sectionKey => $sectionTitle) {
-                $section = $info[$sectionKey];
                 
-                if (isset($section['titles'])) {
-                    $itemList = array();
-                    foreach ($section as $key => $values) {
-                        foreach ($values as $i => $value) {
-                            if (!isset($itemList[$i])) {
-                                $itemList[$i] = array();
-                            }
-                            $listKey = isset($keyRemap[$key]) ? $keyRemap[$key] : $key;
-                            $listValue = $value;
-                            
-                            if ($key == 'infokeys') {
-                                if (isset($infoText['info'][$value])) {
-                                    $listValue = $infoText['info'][$value];
-                                } else {
-                                    Kurogo::log(LOG_ERR, "Transit api-index.ini error: no info section for $value", 'transit');
-                                }
-                            }
-                            
-                            $itemList[$i][$listKey] = $listValue;
-                        }
+                $infoText = $this->getModuleSections('feeds-info');
+                $info = $this->getModuleSections('page-index');
+                $routeConfigs = $view->getRoutes();
+                
+                $agencies = array_fill_keys(array_keys($info['agencies']), false);
+                foreach ($routeConfigs as $routeID => $routeConfig) {
+                    if (!$routeConfig['inService']) {
+                        continue; // only show routes in service
                     }
-                    $results['sections'][] = array(
-                        'key'   => $sectionKey,
-                        'title' => isset($info['infosections'][$sectionKey]) ? $info['infosections'][$sectionKey] : "",
-                        'items' => $itemList,
+                    
+                    $agencyID = $routeConfig['agency'];
+                    $agencies[$agencyID] = array(
+                        'id'    => $agencyID,
+                        'title' => isset($info['agencies'][$agencyID]) ? 
+                            $info['agencies'][$agencyID] : $agencyID,
                     );
                 }
-            }
-
-            $tabs = array();
-            $pageData = $this->getModuleSections('pages');
-            $indexTabs = $pageData['index'];
-            foreach ($indexTabs as $tabID => $tabName) {
-                $tabs[] = array(
-                    # Remove 'tab_'
-                    'id' => substr($tabID, 4),
-                    'title' => $tabName,
+                
+                $results = array(
+                    'agencies' => array_values($agencies),
+                    'sections' => array(),
                 );
-            }
-            $results['tabs'] = $tabs;
-
-            $this->setResponse($results);
-            $this->setResponseVersion($responseVersion);
-            break;
-          
-          case 'stop':
-              $response = array();
-      
-              $stopId = $this->getArg('id');
-              if (!isset($stopId)) {
-                  throw new Exception("Stop id not set");
-              }
-              
-              $stopInfo = $view->getStopInfo($stopId);
-              if (!$stopInfo) {
-                  throw new Exception("No such stop '$stopId'");
-              }
-              
-              $response = $this->formatStopInfo($stopId, $stopInfo, $responseVersion);
-              
-              $this->setResponse($response);
-              $this->setResponseVersion($responseVersion);
-              break;
+                
+                foreach ($info['infosections'] as $sectionKey => $sectionTitle) {
+                    if (!isset($info[$sectionKey]['titles'])) { continue; }
+                    
+                    $section = array(
+                        'key'   => $sectionKey,
+                        'title' => $sectionTitle,
+                        'items' => array(),
+                    );
+                    foreach ($info[$sectionKey]['titles'] as $index => $title) {
+                        $item = array(
+                            'title' => $title,
+                        );
+                        foreach ($keyRemap as $configKey => $itemKey) {
+                            if (isset($info[$sectionKey][$configKey])) {
+                                if ($configKey == 'infokeys') {
+                                    $infoKey = $info[$sectionKey][$configKey][$index];
+                                    if (isset($infoText['info'][$infoKey])) {
+                                        $item[$itemKey] = $infoText['info'][$infoKey];
+                                    } else {
+                                        Kurogo::log(LOG_ERR, "Transit api-index.ini error: no info section for $infoKey", 'transit');
+                                    }
+                                } else {
+                                    $item[$itemKey] = $info[$sectionKey][$configKey][$index];
+                                }
+                            }
+                        }
+                        $section['items'][] = $item;
+                    }
+                    if (count($section['items'])) {
+                        $results['sections'][] = $section;
+                    }
+                }
+                
+                $tabs = array();
+                $pageData = $this->getModuleSections('pages');
+                $indexTabs = $pageData['index'];
+                foreach ($indexTabs as $tabID => $tabName) {
+                    if($this->collapseRouteTabs){
+                        if($tabID == 'tab_running'){
+                            # If collapsing, rename running to routes
+                            $tabID = 'tab_routes';
+                        }
+                    }
+                    $tabs[] = array(
+                        # Remove 'tab_'
+                        'id' => substr($tabID, 4),
+                        'title' => $tabName,
+                    );
+                }
+                $results['tabs'] = $tabs;
+                
+                $this->setResponse($results);
+                $this->setResponseVersion($responseVersion);
+                break;
             
-          case 'routes':
-              $response = array();
-              $routesInfo = $view->getRoutes();
-              foreach ($routesInfo as $routeId => $routeInfo) {
-                  if ($routeInfo['inService']) { // only show routes in service
-                      $response[] = $this->formatBriefRouteInfo($routeId, $routeInfo);
-                  }
-              }
-              
-              $this->setResponse($response);
-              $this->setResponseVersion($responseVersion);
-              break;
-          
-          case 'route':
-              $routeId = $this->getArg('id');
-              
-              if (!$routeId) {
-                  throw new Exception('No route parameter');
-              }
-              
-              $routeInfo = $view->getRouteInfo($routeId);
-              if (!$routeInfo) {
-                  throw new Exception("No such route '$routeId'");
-              }
-              
-              $response = $this->formatFullRouteInfo($routeId, $routeInfo, $responseVersion);
-              
-              // Add route paths (if any)
-              // Note: these line segments are not necessarily a loop
-              $response['paths'] = array_values($view->getRoutePaths($routeId));
-              
-              // Add route vehicles (if any)
-              $response['vehicles'] = array();
-              foreach($view->getRouteVehicles($routeId) as $vehicleId => $vehicleInfo) {
-                  $response['vehicles'][] = $this->formatVehicleInfo($vehicleId, $vehicleInfo);
-              }
-              
-              $this->setResponse($response);
-              $this->setResponseVersion($responseVersion);
-              break;
-          
-          case 'announcements':
-              $response = array();
+            case 'stop':
+                $response = array();
         
-              $newsFeeds = array();
-              $newsConfig = $this->getModuleSections('feeds-news');
-              foreach ($newsConfig as $agencyID => $feedData) {
-                  $modelClass = isset($feedData['MODEL_CLASS']) ? $feedData['MODEL_CLASS'] : $this->defaultNewsModel;
-                  $feed = TransitNewsDataModel::factory($modelClass, $feedData);
-                  if (!$feed) { continue; }
-                  
-                  $feed->setStart(0);
-                  $feed->setLimit(null);
-                  $items = $feed->items();
-                  if ($items) {
-                      if (!isset($response[$agencyID])) {
-                          $heading = isset($newsConfig[$agencyID]['TITLE']) ? 
-                              $newsConfig[$agencyID]['TITLE'] : $agencyID;
-                          
-                          $response[$agencyID] = array(
-                              'announcements' => array(),
-                          );
-                          if ($responseVersion > 1) {
-                              $response[$agencyID]['title'] = $heading;
-                              $response[$agencyID]['agency'] = $agencyID;
-                          } else {
-                              $response[$agencyID]['name'] = $agencyID;
-                          }
-                      }
-                      
-                      foreach ($items as $item) {
-                          $content = $item->getContent();
-                          if (!$content) {
-                              $content = $item->getDescription();
-                          }
-                      
-                          $response[$agencyID]['announcements'][] = array(
-                              'agency' => $agencyID,
-                              'title' => $item->getTitle(),
-                              'date' => $item->getPubDate()->format('Y/m/d'),
-                              'timestamp' => $item->getPubTimestamp(),
-                              'urgent' => false,
-                              'html'  => $content,
-                          );
-                      }
-                  }
-              }
+                $stopId = $this->getArg('id');
+                if (!isset($stopId)) {
+                    throw new Exception("Stop id not set");
+                }
+                
+                $stopInfo = $view->getStopInfo($stopId);
+                if (!$stopInfo) {
+                    throw new Exception("No such stop '$stopId'");
+                }
+                
+                $response = $this->formatStopInfo($stopId, $stopInfo, $responseVersion);
+                
+                $this->setResponse($response);
+                $this->setResponseVersion($responseVersion);
+                break;
               
-              $this->setResponse(array_values($response));
-              $this->setResponseVersion($responseVersion);
-              break;
+            case 'routes':
+                $response = array();
+                $routesInfo = $view->getRoutes();
+                foreach ($routesInfo as $routeId => $routeInfo) {
+                    if ($routeInfo['inService']) { // only show routes in service
+                        $response[] = $this->formatBriefRouteInfo($routeId, $routeInfo);
+                    }
+                }
+                
+                $this->setResponse($response);
+                $this->setResponseVersion($responseVersion);
+                break;
+            
+            case 'route':
+                $routeId = $this->getArg('id');
+                
+                if (!$routeId) {
+                    throw new Exception('No route parameter');
+                }
+                
+                $routeInfo = $view->getRouteInfo($routeId);
+                if (!$routeInfo) {
+                    throw new Exception("No such route '$routeId'");
+                }
+                
+                $response = $this->formatFullRouteInfo($routeId, $routeInfo, $responseVersion);
+                
+                // Add route paths (if any)
+                // Note: these line segments are not necessarily a loop
+                $response['paths'] = array_values($view->getRoutePaths($routeId));
+                
+                // Add route vehicles (if any)
+                $response['vehicles'] = array();
+                foreach($view->getRouteVehicles($routeId) as $vehicleId => $vehicleInfo) {
+                    $response['vehicles'][] = $this->formatVehicleInfo($vehicleId, $vehicleInfo);
+                }
+                
+                $this->setResponse($response);
+                $this->setResponseVersion($responseVersion);
+                break;
+            
+            case 'announcements':
+                $response = array();
           
-          case 'vehicles':
-              $routeId = $this->getArg('id');
-              
-              if (!$routeId) {
-                  throw new Exception('No route parameter');
-              }
-              
-              $routeVehicles = $view->getRouteVehicles($routeId);
-              
-              $response = array();
-              foreach($routeVehicles as $vehicleId => $vehicleInfo) {
-                  $response[] = $this->formatVehicleInfo($vehicleId, $vehicleInfo);
-              }
-              
-              $this->setResponse($response);
-              $this->setResponseVersion($responseVersion);
-              break;
+                $newsFeeds = array();
+                $newsConfig = $this->getModuleSections('feeds-news');
+                foreach ($newsConfig as $agencyID => $feedData) {
+                    $modelClass = isset($feedData['MODEL_CLASS']) ? $feedData['MODEL_CLASS'] : $this->defaultNewsModel;
+                    $feed = TransitNewsDataModel::factory($modelClass, $feedData);
+                    if (!$feed) { continue; }
+                    
+                    $feed->setStart(0);
+                    $feed->setLimit(null);
+                    $items = $feed->items();
+                    if ($items) {
+                        if (!isset($response[$agencyID])) {
+                            $heading = isset($newsConfig[$agencyID]['TITLE']) ? 
+                                $newsConfig[$agencyID]['TITLE'] : $agencyID;
+                            
+                            $response[$agencyID] = array(
+                                'announcements' => array(),
+                            );
+                            if ($responseVersion > 1) {
+                                $response[$agencyID]['title'] = $heading;
+                                $response[$agencyID]['agency'] = $agencyID;
+                            } else {
+                                $response[$agencyID]['name'] = $agencyID;
+                            }
+                        }
+                        
+                        foreach ($items as $item) {
+                            $content = $item->getContent();
+                            if (!$content) {
+                                $content = $item->getDescription();
+                            }
+                        
+                            $response[$agencyID]['announcements'][] = array(
+                                'agency' => $agencyID,
+                                'title' => $item->getTitle(),
+                                'date' => $item->getPubDate()->format('Y/m/d'),
+                                'timestamp' => $item->getPubTimestamp(),
+                                'urgent' => false,
+                                'html'  => $content,
+                            );
+                        }
+                    }
+                }
+                
+                $this->setResponse(array_values($response));
+                $this->setResponseVersion($responseVersion);
+                break;
+            
+            case 'vehicles':
+                $routeId = $this->getArg('id');
+                
+                if (!$routeId) {
+                    throw new Exception('No route parameter');
+                }
+                
+                $routeVehicles = $view->getRouteVehicles($routeId);
+                
+                $response = array();
+                foreach($routeVehicles as $vehicleId => $vehicleInfo) {
+                    $response[] = $this->formatVehicleInfo($vehicleId, $vehicleInfo);
+                }
+                
+                $this->setResponse($response);
+                $this->setResponseVersion($responseVersion);
+                break;
         }
     }
 }
